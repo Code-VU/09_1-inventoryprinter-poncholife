bf17d1a1afa08100fb63136e61827eb8f5a97eb2
poncholife <jonathan.willis@veteransunited.com>
2024-05-12 11:26:51 -0500

    2nd course 2024 05/12/2024

--- displayInventory.py
+++ displayInventory.py
@@ -1,27 +1,13 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12,}
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
-    # your code goes here
-
-    #total_inventory_items = 0
-    # print('Inventory:')
-    # for key,value in inventory.items():
-    #     print(value, key)
-    #     total_inventory_items += value
-    # print('Total number of items:', total_inventory_items)
-
-    #The following code is my original try before watching the video. Updated post video to pass in "inventory" 
-    #not "stuff". This one isn't as clean but it does sort the inventory.  
-    count = 0
+    print('Inventory:')
+    total_items_in_inventory = 0
     for key in inventory:
-        if inventory[key] > 0:
-            count = count + inventory[key]
-    lst = list(inventory.keys())
-    lst.sort()
-    print("Inventory:")
-    for key in lst:
+        total_items_in_inventory += inventory[key]
         print(inventory[key], key)
-    print("Total number of items:", count)
+    print(f'Total number of items: {total_items_in_inventory}')
+
 
 if __name__ == "__main__":
     displayInventory(stuff)
